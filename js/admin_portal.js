8d0b5da8c513793f1f86f3f547492bfc42b3f339
Rohan-uel <u2700818@uel.ac.uk>
2024-12-05 23:16:21 +0000

    Update admin_portal.js

--- js/admin_portal.js
+++ js/admin_portal.js
@@ -1,122 +1,118 @@
 $(document).ready(function() {
-
     var firebaseConfig = {
-    apiKey: "AIzaSyBin1evT-H6jfR49WIhtVPsGMLzbEklIQY",
-    authDomain: "library-management-syste-f2a85.firebaseapp.com",
-    databaseURL: "https://library-management-syste-f2a85.firebaseio.com",
-    projectId: "library-management-syste-f2a85",
-    storageBucket: "library-management-syste-f2a85.appspot.com",
-    messagingSenderId: "914416876417",
-    appId: "1:914416876417:web:bf9e7762c1c283ba"
+        apiKey: "AIzaSyBA2O5kze6cSrMqrUpDYoP5KZepBsNEa4k",
+        authDomain: "library-management-syste-37dc2.firebaseapp.com",
+        databaseURL: "https://library-management-syste-37dc2-default-rtdb.firebaseio.com",
+        projectId: "library-management-syste-37dc2",
+        storageBucket: "library-management-syste-37dc2.firebasestorage.app",
+        messagingSenderId: "962676782645",
+        appId: "1:962676782645:web:c008daf774bcee5a80e37c",
+        measurementId: "G-NZ9VJF0LJ2"
     };
+
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app(); // if already initialized, use that app
+    }
+
     var db = firebase.firestore();
-    
-    //var docRef = db.collection("books");
 
-    // db.collection("books").where("subject", "==", "Computer Science")
-    // .get()
-    // .then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //         // doc.data() is never undefined for query doc snapshots
-    //         console.log(doc.id, " => ", doc.data());
-    //     });
-    // })
-    // .catch(function(error) {
-    //     console.log("Error getting documents: ", error);
-    // });
-
+    // Fetch and display all books
     db.collection("books").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             $('#books').append(
-                "<div><h2>"+doc.data().bookcode+"-" +doc.data().bookname+"</h2>"+
-                "<h3>"+doc.data().author1 + " , " +doc.data().author2 +"</h3>"+
-                "<h3>"+doc.data().subject+"</h3>"+
-                "<p>" + doc.data().tags + "</p>"+
-                "</div><hr>");
+                "<div><h2>" + doc.data().bookcode + " - " + doc.data().bookname + "</h2>" +
+                "<h3>" + doc.data().author1 + " , " + doc.data().author2 + "</h3>" +
+                "<h3>" + doc.data().subject + "</h3>" +
+                "<p>" + doc.data().tags + "</p>" +
+                "</div><hr>"
+            );
         });
     });
 
+    // Fetch and display all users (email ids included)
     db.collection("users").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            var book = [];
-            book = doc.data().books;
-            books_set = "<ul>";
-            for(var i=0;i<book.length;i++)
-            {
-                books_set = books_set +"<li>"+ book[i] + "</li>" ;
-            }
-            books_set = books_set + "</ul>";
+            var book = doc.data().books || [];
+            var books_set = "<ul>";
+            book.forEach(function(b) {
+                books_set += "<li>" + b + "</li>";
+            });
+            books_set += "</ul>";
+
             $('#users').append(
-                "<div><h2>"+ doc.data().name+"</h2>"+
-                "<h3>"+"Roll Number : "+doc.data().Roll_Number+"</h3>"+
-                "<h3>"+"Date of Birth : " + doc.data().DOB +"</h3>"+
-                "<h3>"+"E-mail Id: " + doc.data().Email +"</h3>"+
-                books_set+
-                "</div><hr>");
+                "<div><h2>" + doc.data().name + "</h2>" +
+                "<h3>" + "Roll Number: " + doc.data().Roll_Number + "</h3>" +
+                "<h3>" + "Date of Birth: " + doc.data().DOB + "</h3>" +
+                "<h3>" + "E-mail Id: " + doc.data().Email + "</h3>" +
+                books_set +
+                "</div><hr>"
+            );
         });
     });
-  
+
+    // Prevent default form submission for searches
     $("#Students_search").submit(function(e) {
         e.preventDefault();
     });
-    
+
     $("#Books_search").submit(function(e) {
         e.preventDefault();
     });
 
-    d1 = document.getElementById("books");
-    d2 = document.getElementById("users");
+    // Hide the users and books sections by default
+    var d1 = document.getElementById("books");
+    var d2 = document.getElementById("users");
     d2.style.display = "none";
     d1.style.display = "none";
 
-
-    $('#show_books').click(function()
-    {
+    // Show books section
+    $('#show_books').click(function() {
         show_books();
     });
-    $('#show_students').click(function()
-    {
+
+    // Show students section
+    $('#show_students').click(function() {
         show_students();
     });
 
-    $('#log_button').click(function()
-    {
+    // Log out function
+    $('#log_button').click(function() {
         logout();
     });
-    firebase.auth().onAuthStateChanged(user => {
-        if(!user) {
-            window.location = 'index.html';
-            }
-    });
 
+    // Check if the user is authenticated
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            window.location = 'index.html';
+        }
+    });
 });
 
-function show_books()
-{
-    d1 = document.getElementById("books");
-    d2 = document.getElementById("users");
+// Function to show books
+function show_books() {
+    var d1 = document.getElementById("books");
+    var d2 = document.getElementById("users");
     d2.style.display = "none";
     d1.style.display = "block";
 }
 
-function show_students()
-{
-    d1 = document.getElementById("books");
-    d2 = document.getElementById("users");
+// Function to show students
+function show_students() {
+    var d1 = document.getElementById("books");
+    var d2 = document.getElementById("users");
     d1.style.display = "none";
     d2.style.display = "block";
 }
 
-
-function logout()
-{
+// Logout function
+function logout() {
     firebase.auth().signOut().then(function() {
-        console.log("logout done");
+        console.log("Logout successful");
         window.location = 'admin_login.html';
     }).catch(function(error) {
-        console.log("error");
+        console.log("Error during logout: " + error);
     });
 }
